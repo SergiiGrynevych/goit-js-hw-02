a59332bcb466be8fb859cbd862805b7777559297
SergiiGrynevych <grinevich.sergey.official@gmail.com>
2020-10-16 14:48:34 +0300

    добавил второе возможное решение 8 задачи по JS

--- js/task_8.js
+++ js/task_8.js
@@ -6,14 +6,28 @@
 //   используя цикл for.Для подсчета суммы используй переменную total.
 // Функция должна возвращать 0 если массив пустой и значение total в противном случае.
 
-function reduceArray(array) {
-  let total = 0;
-  for (const item of array) {
-    total <= array.length ? (total += item) : 0;
-  }
 
+/////////// первое решение задачи 
+// function reduceArray(array) {
+//   let total = 0;
+//   for (const item of array) {
+//     total <= array.length ? (total += item) : 0;
+//   }
+
+//   return total;
+// }
+
+/////////// второе решение задачи 
+function reduceArray(array) {
+  'use strict';
+  let total = 0;
+    for(let i = 0; i < array.length; i += 1){
+      if(array.length > 0){
+        total += array[i]
+      }
+  }  
   return total;
-}
+};
 
 console.log(reduceArray([1, 2, 3]));
 // 6
@@ -24,28 +38,29 @@ console.log(reduceArray([-2, 0, 2]));
 console.log(reduceArray([1, 2, 2.5]));
 // 5.5
 
-const hotel = {
-  name: "Resort Hotel",
-  stars: 5,
-  capacity: 100,
-};
+
+// const hotel = {
+//   name: "Resort Hotel",
+//   stars: 5,
+//   capacity: 100,
+// };
 
 /*
  * Посмотрите в консоли из каких двух элементов состоит каждый подмассив.
  * Первый элемент это ключ свойства, второй это значение.
  */
-const entries = Object.entries(hotel);
+// const entries = Object.entries(hotel);
 
 /*
  * На каждой итерации по entries выберем первый элемент подмассива (ключ)
  * в переменную key, а второй (значение) в переменную value
  */
-for (const entry of entries) {
-  const key = entry[0];
-  const value = entry[1];
+// for (const entry of entries) {
+//   const key = entry[0];
+//   const value = entry[1];
 
-  console.log(`${key}: ${value}`);
-}
+//   console.log(`${key}: ${value}`);
+// }
 
 /*
  * name: Resort Hotel
